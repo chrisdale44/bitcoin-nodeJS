6c846e522e62d322ea9c7d1096daade0e4b0873a
Chris Dale <chris.dale@asos.com>
2018-01-26 11:08:12 +0000

    Tut2: use express, output btc price

---
+++ README.md
@@ -0,0 +1,2 @@
+Command to run app:
+node app.js
--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var http = require("http");
+var express = require("express");
+var app = express();
 var request = require("request");
 
-http.createServer(function(req, res){
-    request({
-        url: "https://blockchain.info/stats?format=json",
-        json: true
-    }, function(error, response, body) {
-        console.log(body);
-    });
-    console.log("hi I'm a new bitoin user");
-    res.end("bitcoin to the moon")
-}).listen(8080);
+request({
+    url: "https://blockchain.info/stats?format=json",
+    json: true
+}, function(error, response, body) {
+    btcPrice = body.market_price_usd
+});
+
+app.get("/", function(req, res) {
+    res.send("bitcoin to the moon: $" + btcPrice);
+});
+
+app.listen(8080, function() {
+    console.log("server running")
+});
